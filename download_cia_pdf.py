41cae52c681dda6eb612f069cf698bfd74188e8e
chibadai <aaiiuueeoo19922000t@gmail.com>
2018-08-19 23:17:55 +0900

    complete DL
    complete DL code by python

---
+++ download_cia_pdf.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("prefs", {
+  "download.default_directory": 'D:\GitHub\selenium\selenium_PDF_Downloader\cia',
+  "download.prompt_for_download": False,
+  "download.directory_upgrade": True,
+  "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+  "plugins.always_open_pdf_externally": True
+})
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-print-preview")
+
+driver = webdriver.Chrome(chrome_options=options)
+# driver.get("http://example.com/sample.pdf")
+# for i in range(0, 94015):
+n= None
+try:
+    for i in range(0, 2):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("prefs", {
+          "download.default_directory": 'D:\GitHub\selenium\selenium_PDF_Downloader\cia\\' + str(i),
+          "download.prompt_for_download": False,
+          "download.directory_upgrade": True,
+          "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+          "plugins.always_open_pdf_externally": True
+        })
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-print-preview")
+
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.get('https://www.cia.gov/library/readingroom/document-type/crest?page=' + str(i))
+        tmpTag = driver.find_elements_by_tag_name("a")
+        for i in range(0, len(tmpTag)):
+            print(i)
+            if isinstance(n, type(tmpTag[i].get_attribute('href'))) == False:
+                if tmpTag[i].get_attribute('href')[-4:].find('.pdf') == 0:
+                    print(tmpTag[i].get_attribute('href'))
+                    driver.get(tmpTag[i].get_attribute('href'))
+except:
+    print('error')
+# driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700004-3.pdf')
+# driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700002-5.pdf')
+# driver.quit()
--- download_sample.py
+++ download_sample.py
@@ -1,19 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("prefs", {
-  "download.default_directory": 'D:\GitHub\selenium\selenium_PDF_Downloader\cia',
-  "download.prompt_for_download": False,
-  "download.directory_upgrade": True,
-  "plugins.plugins_disabled": ["Chrome PDF Viewer"],
-  "plugins.always_open_pdf_externally": True
-})
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-print-preview")
-
-driver = webdriver.Chrome(chrome_options=options)
-# driver.get("http://example.com/sample.pdf")
-driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700004-3.pdf')
-driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700002-5.pdf')
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("prefs", {
+#   "download.default_directory": 'D:\GitHub\selenium\selenium_PDF_Downloader\cia\0',
+#   "download.prompt_for_download": False,
+#   "download.directory_upgrade": True,
+#   "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+#   "plugins.always_open_pdf_externally": True
+# })
+# options.add_argument("--disable-extensions")
+# options.add_argument("--disable-print-preview")
+#
+# driver = webdriver.Chrome(chrome_options=options)
+# driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700004-3.pdf')
+# driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700002-5.pdf')
 # driver.quit()
+for i in range(0, 2):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {
+      "download.default_directory": 'D:\GitHub\selenium\selenium_PDF_Downloader\cia\\' + str(i+1),
+      "download.prompt_for_download": False,
+      "download.directory_upgrade": True,
+      "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+      "plugins.always_open_pdf_externally": True
+    })
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-print-preview")
+
+    driver = webdriver.Chrome(chrome_options=options)
+    if i == 0:
+        driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700004-3.pdf')
+    elif i == 1:
+        driver.get('https://www.cia.gov/library/readingroom/docs/CIA-RDP80-00810A002400700002-5.pdf')
